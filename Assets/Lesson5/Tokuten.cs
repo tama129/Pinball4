017285204c9e02205686bd675957d3f3942459ce
tama926 <acta_tamami@icloud.com>
2020-06-25 20:46:35 +0900

    Pinball 4
    再提出

---
+++ Assets/Lesson5/Tokuten.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Tokuten : MonoBehaviour
+{
+    public Text Tensuu;
+    private int score = 0;
+
+    void Start()
+    {
+        score = 0;
+        SetScore();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(score);
+
+        string yourTag = collision.gameObject.tag;
+        if (yourTag == "SmallStarTag")
+        {
+            score += 5;
+        }
+        else if (yourTag == "LargeStarTag")
+        {
+            score += 10;
+        }
+        else if (yourTag == "SmallCloudTag")
+        {
+            score += 20;
+        }
+        else if (yourTag == "LargeCloudTag")
+        {
+            score += 30;
+        }
+
+        Debug.Log(yourTag);
+
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        this.Tensuu.text = score.ToString();
+    }
+}
